e795f816ea1f8d635ebdd36fe7967b49be37966b
Justin Galang <justingalang@gmail.com>
2020-06-05 00:33:20 -0500

    Added OptionCreationPageViewController subclass and set basic functions, datasource, and delegate. Started OptionCreationViewController subclass.

---
+++ BrackIt/Creation Screen/OptionCreationPageViewController.swift
@@ -0,0 +1,95 @@
+//
+//  OptionCreationPageViewController.swift
+//  BrackIt
+//
+//  Created by Justin Galang on 6/4/20.
+//  Copyright © 2020 Justin Galang. All rights reserved.
+//
+
+import UIKit
+
+class OptionCreationPageViewController: UIPageViewController {
+	
+	var optionTextFields : [UITextField]?
+	
+	struct Storyboard {
+		static let optionCreationViewController = "optionCreationViewController"
+	}
+	
+	lazy var controllers: [UIViewController] = {
+		let storyboard = UIStoryboard(name: "Main", bundle: nil)
+		var controllers = [UIViewController]()
+		if let images = self.optionTextFields {
+			for image in images {
+				let shoeImageVC = storyboard.instantiateViewController(identifier: Storyboard.optionCreationViewController)
+				controllers.append(shoeImageVC)
+			}
+		}
+		
+		return controllers
+	}()
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		automaticallyAdjustsScrollViewInsets = false
+		dataSource = self
+		delegate = self
+        
+		self.turnToPage(index: 0)
+    }
+	func turnToPage(index: Int) {
+		let controller = controllers[index]
+		var direction = UIPageViewController.NavigationDirection.forward
+		
+		if let currentVC = viewControllers?.first {
+			let currentIndex = controllers.firstIndex(of: currentVC)!
+			if currentIndex > index {
+				direction = .reverse
+			}
+		}
+		self.configureDisplaying(viewController: controller)
+		
+		setViewControllers([controller], direction: direction, animated: true, completion: nil)
+	}
+	
+	func configureDisplaying(viewController: UIViewController) {
+		for (index, vc) in controllers.enumerated() {
+			if viewController === vc {
+//				if let shoeImageVC = viewController as? OptionCreationPageViewController
+			}
+		}
+	}
+}
+
+extension OptionCreationPageViewController: UIPageViewControllerDataSource {
+	
+	func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+		if let index = controllers.firstIndex(of: viewController) {
+			if index > 0 {
+				return controllers [index - 1]
+			}
+		}
+		return controllers.last
+	}
+	
+	func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+		if let index = controllers.firstIndex(of: viewController) {
+			if index < controllers.count - 1 {
+				return controllers[index + 1]
+			}
+		}
+		return controllers.first
+	}
+}
+
+extension OptionCreationPageViewController : UIPageViewControllerDelegate{
+	func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+		self.configureDisplaying(viewController: pendingViewControllers.first as! OptionCreationPageViewController)
+	}
+	func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+		if !completed {
+			self.configureDisplaying(viewController: previousViewControllers.first as! OptionCreationPageViewController)
+		}
+	}
+}
---
+++ BrackIt/Creation Screen/OptionCreationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OptionCreationViewController.swift
+//  BrackIt
+//
+//  Created by Justin Galang on 6/5/20.
+//  Copyright © 2020 Justin Galang. All rights reserved.
+//
+
+import UIKit
+
+class OptionCreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
